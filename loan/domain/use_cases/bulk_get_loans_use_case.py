974d19d459d539629a964a369fc2336e2d1e6435
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-11-08 21:26:04 +0530

    feat: use case to bulk get loans

---
+++ loan/domain/use_cases/bulk_get_loans_use_case.py
@@ -0,0 +1,14 @@
+from loan.data.abstract_repo import LoanAbstractRepository
+from loan.domain.domain_models import LoanListDomainModel
+from dependency_injector.wiring import Provide
+
+
+class BulkGetLoansUseCase:
+    def __init__(
+        self,
+        db_repo: LoanAbstractRepository = Provide["loan_db_repo"]
+    ) -> None:
+        self.db_repo = db_repo
+
+    def execute(self, customer_id: int) -> LoanListDomainModel:
+        return self.db_repo.bulk_get(customer_id)
