26e8eeb8d3ad7179757f133ece1995766f83c89c
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-11-08 21:23:28 +0530

    feat: add a util function to serialize reponse

---
+++ CreditFlow/utils.py
@@ -0,0 +1,13 @@
+from typing import Dict, Union, cast
+
+from pydantic import BaseModel
+from pydantic.generics import GenericModel
+import orjson
+
+def dict_serialized(obj: Union[BaseModel, GenericModel], *args, **kwargs) -> Dict:
+    """
+    Returns a dict of serialized data. We need this because
+    pydantic's .json() returns stringified json, we need
+    a dict to return it as a response.
+    """
+    return cast(Dict, orjson.loads(obj.json(*args, **kwargs)))
