3ac93ac9c12bdaa6dc76fe75a2978773b1d1c092
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-11-09 09:56:50 +0530

    chore: inject get loan use case in container

--- loan/inject.py
+++ loan/inject.py
@@ -4,6 +4,7 @@ from loan.data.abstract_repo import LoanAbstractRepository
 from loan.data.db_repo import LoanDbRepository
 from loan.domain.use_cases.bulk_get_loans_use_case import BulkGetLoansUseCase
 from loan.domain.use_cases.check_loan_eligibility_use_case import CheckLoanEligibilityUseCase
+from loan.domain.use_cases.get_loan_use_case import GetLoanUseCase
 
 class LoanConainter(containers.DeclarativeContainer):
     loan_db_repo = providers.Dependency(
@@ -12,3 +13,4 @@ class LoanConainter(containers.DeclarativeContainer):
     )
     bulk_get_loans_use_case = providers.Factory(BulkGetLoansUseCase)
     check_loan_eligibility_use_case = providers.Factory(CheckLoanEligibilityUseCase)
+    get_loan_use_case = providers.Factory(GetLoanUseCase)
