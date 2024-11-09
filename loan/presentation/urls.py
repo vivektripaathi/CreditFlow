a37ca7034e3e532accd360af9ba025dea895a447
vivek-tripathi-9005 <vivektripathi8005@gmail.com>
2024-11-09 11:30:50 +0530

    feat: exposed endpoint to create loan
    - Route: "/loan/create-loan/".

--- loan/presentation/urls.py
+++ loan/presentation/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from loan.presentation.views import BulkGetLoanView, CheckLoanEligibilityView, GetLoanView
+from loan.presentation.views import BulkGetLoanView, CheckLoanEligibilityView, CreateLoanView, GetLoanView
 
 urlpatterns = [
     path('view_loans/<int:customer_id>/', BulkGetLoanView.as_view(), name="bulk_get_loans"),
     path('view_loan/<int:loan_id>/', GetLoanView.as_view(), name="get_loans"),
     path('check-eligibility/', CheckLoanEligibilityView.as_view(), name="bulk_loan_eligibility"),
+    path('create-loan/', CreateLoanView.as_view(), name="create_loan"),
 ]
 
